d4967ea5241e362df5b6f58f24b838785385c803
Natalie Simon <admin@nataliesimon.fr>
2026-01-07 23:12:06 +0100

    add docker-compose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from the best of the World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 const PORT = 8080
